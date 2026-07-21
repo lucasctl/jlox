63bcc5fb3babf96c729f0cc0ee3bc92db0c64f49
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2026-07-21 15:06:47 +0200

    Add test for printer

--- src/main/java/com/lucasctl/jlox/AstPrinter.java
+++ src/main/java/com/lucasctl/jlox/AstPrinter.java
@@ -38,14 +38,4 @@ class AstPrinter implements Expr.Visitor<String> {
 
     return builder.toString();
   }
-
-  static void main() {
-    Expr expression =
-        new Expr.Binary(
-            new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
-            new Token(TokenType.STAR, "*", null, 1),
-            new Expr.Grouping(new Expr.Literal(45.67)));
-
-    System.out.println(new AstPrinter().print(expression));
-  }
 }
--- src/main/java/com/lucasctl/jlox/Expr.java
+++ src/main/java/com/lucasctl/jlox/Expr.java
@@ -3,18 +3,22 @@ package com.lucasctl.jlox;
 abstract class Expr {
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
+
     R visitGroupingExpr(Grouping expr);
+
     R visitLiteralExpr(Literal expr);
+
     R visitUnaryExpr(Unary expr);
   }
 
   abstract <R> R accept(Visitor<R> visitor);
+
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
-  }
+    }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -25,10 +29,11 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
+
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
-  }
+    }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -37,10 +42,11 @@ abstract class Expr {
 
     final Expr expression;
   }
+
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
-  }
+    }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -49,11 +55,12 @@ abstract class Expr {
 
     final Object value;
   }
+
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
-  }
+    }
 
     @Override
     <R> R accept(Visitor<R> visitor) {
